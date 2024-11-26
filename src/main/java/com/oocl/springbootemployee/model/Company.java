3a0508b0378e8d38d4d5d72daa3ce0f40600089f
Mason ma <MAMA14@oocl.com>
2024-11-26 18:23:53 +0800

    feat:use jpa refactor repo

--- src/main/java/com/oocl/springbootemployee/model/Company.java
+++ src/main/java/com/oocl/springbootemployee/model/Company.java
@@ -1,15 +1,27 @@
 package com.oocl.springbootemployee.model;
 
+import jakarta.persistence.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Company {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "companyId")
     private List<Employee> employees = new ArrayList<>();
 
     public Company(Integer id, String name, List<Employee> employees) {
         this.id = id;
+        this.name = name;
+        this.employees = employees;
+    }
+
+    public Company(String name, List<Employee> employees) {
         this.name = name;
         this.employees = employees;
     }
@@ -19,7 +31,8 @@ public class Company {
         this.name = name;
     }
 
-    public Company() {}
+    public Company() {
+    }
 
     public Company(String name) {
         this.name = name;
